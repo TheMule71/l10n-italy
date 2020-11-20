a75f124f64067e2c81f3cedff3a37fba8a966024
Marco Colombo <marco.colombo@gmail.com>
2020-11-20 19:53:35 +0100

    [FIX] gestione degli errori

--- l10n_it_fatturapa_in/wizard/efattura.py
+++ l10n_it_fatturapa_in/wizard/efattura.py
@@ -124,7 +124,8 @@ def CreateFromDocument(xml_string):
         def __setitem__(self, *attr, **kwattr):
             return self.__dict__.__setitem__(*attr, **kwattr)
 
-    validator = xmlschema.XMLSchema(_xsd_schema)  # TODO: crearlo una tantum?
+    # TODO: crearlo una tantum?
+    validator = xmlschema.XMLSchema(_xsd_schema, validation='lax')
 
     xml_string = _fix_xmlstring(xml_string)
     root = etree.fromstring(xml_string)
@@ -162,8 +163,8 @@ def CreateFromDocument(xml_string):
                     problems.append(msg)
                     _logger.warn(msg)
 
-    validat = validator.to_dict(tree, dict_class=ObjectDict, decimal_type=str)
-    setattr(validat, '_xmldoctor', problems)
+    validat, errors = validator.to_dict(tree, dict_class=ObjectDict)
+    setattr(validat, '_xmldoctor', problems + errors)
     return validat
 
 
