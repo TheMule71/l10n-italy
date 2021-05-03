150301e7c0fc2711e747ed687a470a3dba2ac49f
tafaRU <alex.comba@agilebg.com>
2021-05-03 17:24:38 +0200

    Replace pre-migration script by pre_init_hook in order to properly perform the module renaming.

--- l10n_it_withholding_tax_reason/__init__.py
+++ l10n_it_withholding_tax_reason/__init__.py
@@ -1,1 +1,20 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2021 Alex Comba - Agile Business Group
+
 from . import models
+
+from openupgradelib import openupgrade
+
+
+def rename_old_italian_module(cr):
+
+    if not openupgrade.is_module_installed(cr, "l10n_it_withholding_tax_causali"):
+        return
+
+    openupgrade.update_module_names(
+        cr,
+        [
+            ("l10n_it_withholding_tax_causali", "l10n_it_withholding_tax_reason"),
+        ],
+        merge_modules=True,
+    )
