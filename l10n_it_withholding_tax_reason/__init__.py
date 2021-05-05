ee1692f0dfd9128a9b79f19489fbba1fb7aedc8d
tafaRU <alex.comba@agilebg.com>
2021-05-05 12:36:44 +0200

    rename_fields needs to be executed in Odoo shell

--- l10n_it_withholding_tax_reason/__init__.py
+++ l10n_it_withholding_tax_reason/__init__.py
@@ -11,6 +11,16 @@ def rename_old_italian_module(cr):
     if not openupgrade.is_module_installed(cr, "l10n_it_withholding_tax_causali"):
         return
 
+    openupgrade.rename_xmlids(
+        cr,
+        [
+            (
+                "l10n_it_withholding_tax_causali.view_withholding_tax_form_causale",
+                "l10n_it_withholding_tax_causali.view_withholding_tax_form_reason",
+            ),
+        ],
+    )
+
     openupgrade.update_module_names(
         cr,
         [
