410c3298456f2bb9e571ea7aa127fae31d17074d
Marco Colombo <marco.colombo@gmail.com>
2022-01-21 14:43:26 +0100

    internal use

--- supermerge.sh
+++ supermerge.sh
@@ -32,31 +32,24 @@ fi
 
 set -xe
 rebase_n_merge https://github.com/odoo-italia/l10n-italy 14.0-premerge 14.0-premerge
+
 # fixes
-# rebase_n_merge_pr 2285
-rebase_n_merge_pr 2327
-rebase_n_merge_pr 2346
-rebase_n_merge_pr 2425
-rebase_n_merge_pr 2430
-rebase_n_merge_pr 2453
-rebase_n_merge_pr 2468
-rebase_n_merge_pr 2476
-rebase_n_merge_pr 2479
+rebase_n_merge_pr 2539
+rebase_n_merge_pr 2553
+rebase_n_merge_pr 2563
+rebase_n_merge_pr 2571
+rebase_n_merge_pr 2572
+rebase_n_merge_pr 2584
+rebase_n_merge_pr 2595
+rebase_n_merge_pr 2602
+rebase_n_merge_pr 2604
+
 # porting
-rebase_n_merge_pr 2310
-rebase_n_merge_pr 2138
-rebase_n_merge_pr 2139
 rebase_n_merge_pr 2149
-rebase_n_merge_pr 2151
-rebase_n_merge_pr 2157
 rebase_n_merge_pr 2198
 rebase_n_merge_pr 2202
-rebase_n_merge_pr 2212
-rebase_n_merge_pr 2228
-rebase_n_merge_pr 2229
 rebase_n_merge_pr 2258
-rebase_n_merge_pr 2259
-rebase_n_merge_pr 2315
-rebase_n_merge https://github.com/OmniaGit/l10n-italy 14.0-mig-l10n_it_location_nuts 14.0-mig-l10n_it_location_nuts
 rebase_n_merge_pr 2386
-rebase_n_merge_pr 2438
+rebase_n_merge_pr 2570
+rebase_n_merge_pr 2574
+rebase_n_merge https://github.com/OmniaGit/l10n-italy 14.0-mig-l10n_it_location_nuts 14.0-mig-l10n_it_location_nuts
