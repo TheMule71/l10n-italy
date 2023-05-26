e3618cf2d606c0815e27fa5d28fe214e32bed085
Borruso <gborruso@dinamicheaziendali.it>
2023-05-26 14:52:27 +0200

    [MIG] l10n_it_fatturapa_in_purchase: Migration to 16.0

--- l10n_it_fatturapa_in_purchase/models/purchase_order.py
+++ l10n_it_fatturapa_in_purchase/models/purchase_order.py
@@ -2,18 +2,17 @@
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
 # Copyright 2019 Simone Rubino - Agile Business Group
 
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.tools import float_compare
 
 
 class POLine(models.Model):
     _inherit = "purchase.order.line"
-    to_invoice = fields.Boolean(
-        "To Invoice", compute="_compute_qty_invoiced", store=True
-    )
+
+    to_invoice = fields.Boolean(compute="_compute_qty_invoiced", store=True)
 
     def _compute_qty_invoiced(self):
-        super()._compute_qty_invoiced()
+        res = super()._compute_qty_invoiced()
         precision = self.env["decimal.precision"].precision_get(
             "Product Unit of Measure"
         )
@@ -22,7 +21,9 @@ class POLine(models.Model):
                 line.to_invoice = True
             else:
                 line.to_invoice = False
+        return res
 
+    @api.depends("order_id.name", "name")
     def name_get(self):
         res = []
         for line in self:
