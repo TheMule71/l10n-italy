595eec5848f0f1d3c0595fc25493d9721fa1132e
michelerusti <michele.rusticucci@agilebg.com>
2023-06-22 15:36:42 +0200

    [16.0][FIX] Remove unused css classes

--- account_vat_period_end_statement/__manifest__.py
+++ account_vat_period_end_statement/__manifest__.py
@@ -32,7 +32,7 @@
     ],
     "assets": {
         "web.assets_backend": [
-            "account_vat_period_end_statement/static/src/css/l10n_it_vat_registries.css",
+            "account_vat_period_end_statement/static/src/css/*.css",
         ],
     },
     "installable": True,
